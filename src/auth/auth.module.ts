64e37750411902fda4ce989cdff761880265b77b
devopscomputer <pauloti.engenharia@gmail.com>
2026-02-06 20:31:34 -0300

    update: update src/auth/auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,9 @@ import { FacebookAuthController } from './facebook-auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config'; // Importe do pacote oficial
+import { ConfigModule as NestConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from 'src/config/config.module';
+import { PrismaModule } from 'src/prisma/prisma.module'; // Importe do pacote oficial
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
@@ -14,10 +16,12 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     UsersModule,
     PassportModule,
     // Importa o ConfigModule para que o ConfigService esteja disponível
+    NestConfigModule,
     ConfigModule,
+    PrismaModule,
     JwtModule.registerAsync({
       // Importa o ConfigModule aqui para que o useFactory tenha acesso ao ConfigService
-      imports: [ConfigModule],
+      imports: [NestConfigModule],
       useFactory: (configService: ConfigService) => ({
         // Use o método 'get' do ConfigService para acessar as variáveis
         secret: configService.get<string>('JWT_SECRET'),
@@ -33,3 +37,4 @@ import { JwtStrategy } from './strategies/jwt.strategy';
   exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
+
