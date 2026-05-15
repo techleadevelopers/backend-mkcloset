2b74664c198673dd541f6f1c36def0c44921bacb
devopscomputer <pauloti.engenharia@gmail.com>
2026-05-14 21:53:27 -0300

    chore: update src/auth/dto/reset-password.dto.ts

---
+++ src/auth/dto/reset-password.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class ResetPasswordDto {
+  @IsString()
+  @IsNotEmpty()
+  token: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6, { message: 'A senha deve ter no minimo 6 caracteres' })
+  password: string;
+}
